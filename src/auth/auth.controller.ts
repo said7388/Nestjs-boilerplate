74aa4783b1112ac01d8efd4c5bda6da39c5a356a
said7388 <mdabusaidriyaz@gmail.com>
2022-09-09 15:05:13 +0600

    Add package

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,10 +7,12 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
+@ApiTags('Auth Route')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
@@ -6,6 +9,18 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.useGlobalPipes(new ValidationPipe());
+
+  // OpenAPI swagger configuration
+  const options = new DocumentBuilder()
+    .setTitle('Nest-Boilerplate')
+    .setDescription('NestJs Boilerplate API Documentation!')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
